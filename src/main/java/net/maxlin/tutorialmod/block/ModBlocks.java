ec44c2a72bcdb871d8def2a2526e6ade1a675c89
Max <max.ylin@outlook.com>
2025-06-30 12:40:43 -0400

    Finished 5th video

--- src/main/java/net/maxlin/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/maxlin/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ package net.maxlin.tutorialmod.block;
    import net.maxlin.tutorialmod.PikachuInvasionAndMore;
    import net.minecraft.block.AbstractBlock;
    import net.minecraft.block.Block;
+   import net.minecraft.block.ExperienceDroppingBlock;
    import net.minecraft.item.BlockItem;
    import net.minecraft.item.Item;
    import net.minecraft.item.ItemGroups;
@@ -11,19 +12,31 @@ package net.maxlin.tutorialmod.block;
    import net.minecraft.registry.Registry;
    import net.minecraft.sound.BlockSoundGroup;
    import net.minecraft.util.Identifier;
+   import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 
-   public class ModBlocks {
+public class ModBlocks {
        public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
-                  new Block(AbstractBlock.Settings.create().strength(3f)
+                  new Block(AbstractBlock.Settings.create().strength(4f)
                     .requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
        public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
-               new Block(AbstractBlock.Settings.create().strength(4f)
+               new Block(AbstractBlock.Settings.create().strength(3f)
                        .requiresTool()));
 
+       public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+               new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                       AbstractBlock.Settings.create().strength(3f).requiresTool()));
 
-           private static Block registerBlock(String name, Block block){
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+
+
+
+
+    private static Block registerBlock(String name, Block block){
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, Identifier.of(PikachuInvasionAndMore.MOD_ID, name), block);
     }
