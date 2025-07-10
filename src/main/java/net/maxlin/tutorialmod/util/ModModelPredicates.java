26ee9bbc0b9a722e29f7e132e54d66763cd758f5
Max <max.ylin@outlook.com>
2025-07-10 16:43:08 -0400

    Finished 21th video!

---
+++ src/main/java/net/maxlin/tutorialmod/util/ModModelPredicates.java
@@ -0,0 +1,14 @@
+package net.maxlin.tutorialmod.util;
+
+import net.maxlin.tutorialmod.PikachuInvasionAndMore;
+import net.maxlin.tutorialmod.component.ModDataComponentTypes;
+import net.maxlin.tutorialmod.item.ModItems;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.util.Identifier;
+
+public class ModModelPredicates {
+    public static void registerModelPredicates() {
+        ModelPredicateProviderRegistry.register(ModItems.CHISEL, Identifier.of(PikachuInvasionAndMore.MOD_ID, " used"),
+        (stack, world, entity, seed) -> stack.get(ModDataComponentTypes.COORDINATES) !=null ? 1f : 0f);
+    }
+}
