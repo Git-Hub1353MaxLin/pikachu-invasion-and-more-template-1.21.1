90982025c3e8d03a5487f5d85e19411ccd087906
Max <max.ylin@outlook.com>
2025-08-20 15:32:19 -0400

    Finished 28th video!

--- src/main/java/net/maxlin/tutorialmod/item/custom/MangoJuiceItem.java
+++ src/main/java/net/maxlin/tutorialmod/item/custom/MangoJuiceItem.java
@@ -7,8 +7,11 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
 import net.minecraft.world.World;
 
@@ -32,6 +35,11 @@ public class MangoJuiceItem extends Item{
     @Override
     public int getMaxUseTime(ItemStack stack, LivingEntity user) {
         return 32;
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        return ItemUsage.consumeHeldItem(world, user, hand);
     }
 
     @Override
---
+++ src/main/java/net/maxlin/tutorialmod/item/custom/StaffOfPikachuXVIII.java
@@ -0,0 +1,73 @@
+package net.maxlin.tutorialmod.item.custom;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LightningEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.hit.BlockHitResult;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+
+public class StaffOfPikachuXVIII extends Item {
+
+    public StaffOfPikachuXVIII (Settings settings) {
+        super(settings.maxCount(1).maxDamage(128)); // 128 uses
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack stack = user.getStackInHand(hand);
+
+        if (!world.isClient) {
+            // Raycast where the player is looking
+            HitResult hit = raycastFromPlayer(world, user, RaycastContext.FluidHandling.NONE);
+
+            if (hit.getType() == HitResult.Type.BLOCK) {
+                BlockHitResult blockHit = (BlockHitResult) hit;
+                BlockPos pos = blockHit.getBlockPos().up();
+
+                // Spawn lightning
+                LightningEntity lightning = EntityType.LIGHTNING_BOLT.create(world);
+                if (lightning != null) {
+                    lightning.refreshPositionAfterTeleport(
+                            pos.getX() + 0.5,
+                            pos.getY(),
+                            pos.getZ() + 0.5
+                    );
+                    world.spawnEntity(lightning);
+                }
+
+                // Increase durability
+                stack.setDamage(stack.getDamage() + 1);
+
+                // Play break sound if fully damaged
+                if (stack.getDamage() >= stack.getMaxDamage()) {
+                    world.playSound(null, user.getBlockPos(), SoundEvents.ENTITY_ITEM_BREAK, SoundCategory.PLAYERS, 1.0f, 1.0f);
+                }
+
+                // Add cooldown (40 ticks = 2 seconds)
+                user.getItemCooldownManager().set(this, 40);
+            }
+        }
+
+        return TypedActionResult.success(stack, world.isClient());
+    }
+
+    // Raycast for aiming the staff
+    private HitResult raycastFromPlayer(World world, PlayerEntity player, RaycastContext.FluidHandling fluidHandling) {
+        Vec3d start = player.getCameraPosVec(1.0F);
+        Vec3d direction = player.getRotationVec(1.0F).multiply(50); // 50 block range
+        Vec3d end = start.add(direction);
+
+        return world.raycast(new RaycastContext(start, end, RaycastContext.ShapeType.OUTLINE, fluidHandling, player));
+    }
+}
+
