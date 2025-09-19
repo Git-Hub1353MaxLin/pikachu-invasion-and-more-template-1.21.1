734314432aedf369db4390ab75eef185d4e70cef
Max <max.ylin@outlook.com>
2025-09-19 18:14:56 -0400

    Finished 42th video!

--- src/main/java/net/maxlin/tutorialmod/entity/client/MantisRenderer.java
+++ src/main/java/net/maxlin/tutorialmod/entity/client/MantisRenderer.java
@@ -1,21 +1,33 @@
 package net.maxlin.tutorialmod.entity.client;
 
+import com.google.common.collect.Maps;
 import net.maxlin.tutorialmod.PikachuInvasionAndMore;
 import net.maxlin.tutorialmod.entity.custom.MantisEntity;
+import net.maxlin.tutorialmod.entity.custom.MantisVariant;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.EntityRendererFactory;
 import net.minecraft.client.render.entity.MobEntityRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Util;
+
+import java.util.Map;
 
 public class MantisRenderer extends MobEntityRenderer<MantisEntity, MantisModel<MantisEntity>> {
+    private static final Map<MantisVariant, Identifier> LOCATION_BY_VARIANT =
+            Util.make(Maps.newEnumMap(MantisVariant.class), map -> {
+                map.put(MantisVariant.DEFAULT,
+                        Identifier.of(PikachuInvasionAndMore.MOD_ID, "textures/entity/mantis/mantis.png"));
+                map.put(MantisVariant.ORCHID,
+                        Identifier.of(PikachuInvasionAndMore.MOD_ID, "textures/entity/mantis/mantis_orchid.png"));
+            });
     public MantisRenderer(EntityRendererFactory.Context context) {
         super(context, new MantisModel<>(context.getPart(MantisModel.MANTIS)), 0.75f);
     }
 
     @Override
     public Identifier getTexture(MantisEntity entity) {
-        return Identifier.of(PikachuInvasionAndMore.MOD_ID, "textures/entity/mantis/mantis.png");
+        return LOCATION_BY_VARIANT.get(entity.getVariant());
     }
 
     @Override
